2d06964c4db990d621f8ef97164d55362236648e
anwar <mahmoudanwarr10000@gmail.conm>
2022-06-10 11:58:22 +0200

    adminDb
    add user / add machine

--- Task-Manger/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Task-Manger/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 declare var $:any //declear $ to use jquery
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,16 +7,33 @@ declare var $:any //declear $ to use jquery
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  userForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    type: new FormControl('')
+  })
+  machineForm = new FormGroup({
+    machineName: new FormControl(''),
+    password: new FormControl(''),
+  })
+  userFormValue:any;
+  machineFormValue:any;
+
 
   constructor() { }
 
   ngOnInit(): void {
     $('.userIcon').click(function(){
       $('.chooser').css("left","0px")
+      $('.userDiv').css("display", "none");
+      $('.machineDiv').toggleClass('d-none');
+      $('.userDiv').removeClass('d-none');
 
   });
   $('.machineIcon').click(function(){
     $('.chooser').css("left","200px")
+    $('.userDiv').toggleClass('d-none');
+    $('.machineDiv').removeClass('d-none');
     
   });
   $('.checkIcon').click(function(){
@@ -25,6 +43,14 @@ export class AdminDashboardComponent implements OnInit {
   });
   }
 
+  addUser(){
+    console.log(this.userForm.value);
+
+  }
+  addMachine(){
+    console.log(this.machineForm.value);
+
+  }
   
 
 }
