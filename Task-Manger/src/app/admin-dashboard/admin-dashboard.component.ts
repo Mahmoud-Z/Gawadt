79384518ba2e44c2d2b42ed1d11372266037a1ea
anwar <mahmoudanwarr10000@gmail.conm>
2022-06-11 12:04:35 +0200

    adminDb

--- Task-Manger/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Task-Manger/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+export interface Permissions {
+  id: number;
+  name: string;
+}
 declare var $:any //declear $ to use jquery
 @Component({
   selector: 'app-admin-dashboard',
@@ -10,7 +15,9 @@ export class AdminDashboardComponent implements OnInit {
   userForm = new FormGroup({
     userName: new FormControl(''),
     password: new FormControl(''),
-    type: new FormControl('')
+    type: new FormControl(''),
+    permission: this.fb.array([])
+
   })
   machineForm = new FormGroup({
     machineName: new FormControl(''),
@@ -18,9 +25,29 @@ export class AdminDashboardComponent implements OnInit {
   })
   userFormValue:any;
   machineFormValue:any;
+  permissionData: Permissions[] = [
+    { id: 0, name: 'Create/Edit User' },
+    { id: 1, name: 'Create/Edit Machine' },
+    { id: 2, name: 'Create/Edit Task' },
+    { id: 3, name: 'Reports' },
+    { id: 4, name: 'Permission' }
+
+  ];
 
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  onChange(name: string, isChecked: boolean) {
+    const permission = (this.userForm.controls.permission as FormArray);
+
+    if (isChecked) {
+      permission.push(new FormControl(name));
+    } else {
+      const index = permission.controls.findIndex(x => x.value === name);
+      permission.removeAt(index);
+    }
+  }
+
 
   ngOnInit(): void {
     $('.userIcon').click(function(){
