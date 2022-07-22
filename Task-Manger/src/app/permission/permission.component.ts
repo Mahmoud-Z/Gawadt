fb3252b1c18b8c5e82a86a56853a09ac12cab801
anwar <mahmoudanwarr10000@gmail.conm>
2022-07-22 15:56:15 +0200

    permissions

--- Task-Manger/src/app/permission/permission.component.ts
+++ Task-Manger/src/app/permission/permission.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { HomeComponent } from '../home/home.component';
+import jwt from 'jwt-decode';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 
@@ -10,12 +12,17 @@ import { HomeComponent } from '../home/home.component';
   styleUrls: ['./permission.component.css']
 })
 export class PermissionComponent implements OnInit {
-
   Permissions:any;
+  userPrivlage: any;
+  username:any;
+  permissionResponseForm = new FormGroup({
+    responseMessage: new FormControl(''),
+    username: new FormControl(''),
 
-
+  })
 
   constructor(public Service:ApiService,public home:HomeComponent) {
+
     this.getPermssion()
 
    }
@@ -23,13 +30,26 @@ export class PermissionComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  Accept(id:any,type:any,permissionId:any){
-    console.log(id,type,permissionId);
+  Accept(id:any,type:any,permissionId:any,permissionUsername:any){
+    this.permissionResponseForm.value['username']=permissionUsername;
     if (type=='Stop the machine') {
       this.Service.postFun('stopMachine',{id}).subscribe(data => {
       })
+      this.permissionResponseForm.value['responseMessage']='Your Request For Stopping The Machine have been Accepted';
+      this.Service.postFun('permissionResponse',this.permissionResponseForm.value).subscribe(data => {
+      }) 
+    }
+    else if (type=='Start the machine') {
+      this.Service.postFun('startMachine',{id}).subscribe(data => {
+      })
+      this.permissionResponseForm.value['responseMessage']='Your Request For Starting The Machine have been Accepted';
+      this.Service.postFun('permissionResponse',this.permissionResponseForm.value).subscribe(data => {
+      }) 
     }
     else if (type=='Transfer the task') {
+      this.permissionResponseForm.value['responseMessage']='Your Request For Transfering The Task have been Accepted';
+      this.Service.postFun('permissionResponse',this.permissionResponseForm.value).subscribe(data => {
+      }) 
     }
     this.Service.postFun('submittedColumn',{permissionId}).subscribe(data => {
       console.log(data);
@@ -38,15 +58,27 @@ export class PermissionComponent implements OnInit {
       this.home.getTasks();
     })
   }
-
+  Decline(id:any,type:any,permissionId:any,permissionUsername:any){
+    this.permissionResponseForm.value['responseMessage']='Your Request For Stopping The Machine have been Denied';
+    this.Service.postFun('permissionResponse',this.permissionResponseForm.value).subscribe(data => {
+    }) 
+    this.Service.postFun('submittedColumn',{permissionId}).subscribe(data => {
+      this.getPermssion();
+      this.home.getMachines();
+      this.home.getTasks();
+    })
+  }
   getPermssion(){
     this.Service.getFun('getPermissions').subscribe(data => {
       this.Permissions=data
       for (let i = 0; i < this.Permissions.length; i++) {
               
-      if (this.Permissions[i].type =='machinePermission') {
+      if (this.Permissions[i].type =='machinePermissionStop') {
         this.Permissions[i].type='Stop the machine'
         
+      } 
+      else if (this.Permissions[i].type =='machinePermissionStart') {
+        this.Permissions[i].type='Start the machine'
       }
       else if (this.Permissions[i].type =='taskPermission') {
         this.Permissions[i].type='Transfer the task'
